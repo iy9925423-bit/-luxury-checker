de37da91a792238a2fe355b84b71faeb04c372e4
iy9925423-bit <iy9925423@gmail.com>
2026-08-02 04:30:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 function check(){
 
-  let brand = document.getElementById("brand").value;
-  let text = document.getElementById("text").value;
-  let result = document.getElementById("result");
-
-  if(!brand){
-    result.innerHTML = "브랜드를 입력하세요.";
-    return;
-  }
-
-  result.innerHTML = "분석 중...";
-
-  setTimeout(function(){
-
-    result.innerHTML =
-    "분석 완료<br><br>" +
-    "브랜드: " + brand + "<br>" +
-    "정품 가능성 분석 결과입니다.<br>" +
-    "(참고용 결과이며 실제 감정과 다를 수 있습니다.)";
-
-  },1000);
+document.getElementById("result").innerHTML =
+"분석 완료";
 
 }
